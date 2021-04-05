e2453aeff23b84e7deab2b9f30c1a9e7d716bc21
Luís Antônio <codenads@outlook.com>
2021-04-04 23:03:36 -0300

    Safety commit

--- LexicalAnalyzer.py
+++ LexicalAnalyzer.py
@@ -1,35 +1,45 @@
 from FileReader import FileReader
 from Token import TOKENS, TokenCategory, Token
+from CustomType import custom_type
 
 
 class LexicalAnalyzer:
     def __init__(self, file):
         self.file_reader = FileReader(file)
+        self.word = ""
         self.remaining_token_start = 0
 
     def next_token(self):
-        word = self.file_reader.read()
-        token_column = self.file_reader.column - len(word)
+        if not self.remaining_token_start:
+            self.word = self.file_reader.read()
+
+        if not self.word:
+            return None
+
+        token_column = self.file_reader.column - len(self.word)
         start = self.remaining_token_start
 
-        print(word, start)
+        # print(word, start)
 
-        if not word:
-            return None
-        if word in TOKENS.keys():
-            return Token(self.file_reader.line, token_column, word, TokenCategory[TOKENS[word]])
-        elif word[start: start+2] in TOKENS.keys():
-            return Token(self.file_reader.line, token_column, word[start: start+2], TokenCategory[TOKENS[word[start: start+2]]])
-        elif word[start] in TOKENS.keys():
-            return Token(self.file_reader.line, token_column, word[start], TokenCategory[TOKENS[word[start]]])
+        if self.word in TOKENS.keys():
+            self.remaining_token_start = 0
+            return Token(self.file_reader.line, token_column, self.word, TokenCategory[TOKENS[self.word]])
+        elif self.word[start: start+2] in TOKENS.keys():
+            self.remaining_token_start += 2
+            return Token(self.file_reader.line, token_column, self.word[start: start+2], TokenCategory[TOKENS[self.word[start: start+2]]])
+        elif self.word[start] in TOKENS.keys():
+            self.remaining_token_start += 1
+            return Token(self.file_reader.line, token_column, self.word[start], TokenCategory[TOKENS[self.word[start]]])
         else:
             end = start + 1
-            while end < len(word):
-                print(word[start: end])
-                if word[end] in TOKENS.keys() or word[end: end+2] in TOKENS.keys():
+            while end < len(self.word):
+                if self.word[end] in TOKENS.keys() or self.word[end: end+2] in TOKENS.keys() and self.word[end: end+2] != 'or':
                     self.remaining_token_start = end
                     break
                 end += 1
 
-            if word[start: end] in TOKENS.keys():
-                return Token(self.file_reader.line, token_column + start, TokenCategory[TOKENS[word[start: end]]])
+            if self.word[start: end] in TOKENS.keys():
+                return Token(self.file_reader.line, token_column + start, self.word[start: end], TokenCategory[TOKENS[self.word[start: end]]])
+            else:
+                typeof = custom_type(self.word[start: end])
+                return Token(self.file_reader.line, token_column + start, self.word[start: end], TokenCategory[typeof])
